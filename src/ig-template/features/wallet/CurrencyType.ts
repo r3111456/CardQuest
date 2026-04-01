0ee5ea7fb0cba519216b040f5ce1ad8817ff9e21
桃子果醬涼麵 <92939444+r3111456@users.noreply.github.com>
2026-04-02 00:53:41 +0800

    Add files via upload

---
+++ src/ig-template/features/wallet/CurrencyType.ts
@@ -0,0 +1,9 @@
+export enum CurrencyType {
+    Money = "Money",
+    Souls = "Souls",
+    Wood = "Wood",
+    Grain = "Grain",
+    Bronze = "Bronze",
+    Silver = "Silver",
+    Gold = "Gold",
+}
---
+++ src/ig-template/features/wallet/WalletSaveData.ts
@@ -0,0 +1,5 @@
+import {SaveData} from "@/ig-template/tools/saving/SaveData";
+
+export interface WalletSaveData extends SaveData {
+    money: number;
+}
